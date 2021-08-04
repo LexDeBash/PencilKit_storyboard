c7d11ee17ae713c504797c1ad1fcdd43c1592351
yaSkazalGorbatiy <ya.skazal.gorbatiy@gmail.com>
2021-08-04 14:10:52 +0300

    ver.1

---
+++ PencilKit/Controllers/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  PencilKit
+//
+//  Created by Сергей Горбачёв on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var canvas: Canvas!
+    @IBOutlet weak var slider: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        slider.addTarget(
+            self,
+            action: #selector(sliderChangeValue),
+            for: .valueChanged
+        )
+    }
+    
+    @IBAction func colorButtonTapped(_ sender: UIButton) {
+        canvas.setStrokeColor(color: sender.backgroundColor ?? .black)
+        slider.tintColor = sender.backgroundColor ?? .black
+        slider.thumbTintColor = sender.backgroundColor ?? .black
+    }
+
+    @IBAction func undoButtonTapped(_ sender: Any) {
+        canvas.undo()
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: Any) {
+        canvas.clear()
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        StorageManager().saveImage(canvasView: canvas)
+    }
+    
+    @objc private func sliderChangeValue() {
+        canvas.setStrokeWidth(width: slider.value)
+    }
+}
+
---
+++ PencilKit/Model/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  Line
+//
+//  Created by Сергей Горбачёв on 04.08.2021.
+//
+
+import UIKit
+
+struct Line {
+    let strokeWidth: Float
+    let color: UIColor
+    var points: [CGPoint]
+}
+
